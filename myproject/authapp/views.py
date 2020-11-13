48febe37c71a4bfdfbf79dfbae1c91e2987d5a4d
EvgenDEP1 <jen_dostovalov@mail.ru>
2020-11-14 00:01:01 +0500

    my

--- myproject/authapp/views.py
+++ myproject/authapp/views.py
@@ -1,13 +1,44 @@
+import django.contrib.auth as auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-from authapp.forms import LoginForm
+from authapp.forms import LoginForm, RegisterForm
 
 
 def login(request):
-    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            auth.login(request, form.get_user())
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = LoginForm()
 
     context = {
         'form': form,
         'page_title': 'авторизация',
     }
     return render(request, 'authapp/login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main:index'))
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        form = RegisterForm()
+
+    context = {
+        'page_title': 'регистрация',
+        'form': form,
+    }
+    return render(request, 'authapp/register.html', context)
+
