003cf608e921bf0c84dc99f8ff881be0c86a078e
EvgenDEP1 <jen_dostovalov@mail.ru>
2020-11-16 09:46:17 +0500

    my basket

---
+++ myproject/static/js/basket.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    console.log("DOM loaded");
+    $('.basket li a').on('click', function (event) {
+        event.preventDefault();
+        // console.log(event.target.href;
+        $.ajax({
+            url: event.target.href,
+            success: function (data) {
+                // console.log('response', data);
+                $('.basket-item-' + data.book_basket_id).remove();
+            }
+        })
+
+    })
+}
